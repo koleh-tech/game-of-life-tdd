05ad9d2d905860d3f44618c6e0888c5e050b8253
koleh.david <koleh.david@aerometrex.com.au>
2025-05-20 15:20:01 +0930

    t pass

--- spec/cell-logic.spec.ts
+++ spec/cell-logic.spec.ts
@@ -1,6 +1,6 @@
 describe("nextCellStateForCell", () => {
 	it("should return the next state for a cell", () => {
 		// expect(nextCellStateForCell(0, 0, 0)).toBe(1);
-		expect(2).toBe(1)
+		expect(1).toBe(1)
 	})
 })
