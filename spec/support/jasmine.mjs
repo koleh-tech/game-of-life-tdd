b67279e38d6ad57f309d03b1af01886447d901a6
koleh.david <koleh.david@aerometrex.com.au>
2025-05-20 15:49:37 +0930

    restructure

--- spec/support/jasmine.mjs
+++ spec/support/jasmine.mjs
@@ -1,14 +1,14 @@
 export default {
-  spec_dir: "spec",
-  spec_files: [
-    "**/*[sS]pec.ts"
-  ],
-  helpers: [
-    "helpers/**/*.?(m)js"
-  ],
-  env: {
-    stopSpecOnExpectationFailure: false,
-    random: true,
-    forbidDuplicateNames: true
-  }
+    "spec_dir": "build",
+    "spec_files": [
+        "**/*[sS]pec.?(m)js"
+    ],
+    helpers: [
+        "helpers/**/*.?(m)js"
+    ],
+    env: {
+        stopSpecOnExpectationFailure: false,
+        random: true,
+        forbidDuplicateNames: true
+    }
 }
