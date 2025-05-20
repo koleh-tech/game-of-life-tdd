b7261cd987e82c1272f49d38915a86295e5fef77
koleh.david <koleh.david@aerometrex.com.au>
2025-05-20 15:41:14 +0930

    temp

--- spec/cell-logic.spec.ts
+++ spec/cell-logic.spec.ts
@@ -1,6 +1,13 @@
 describe("nextCellStateForCell", () => {
-	it("should return the next state for a cell", () => {
-		// expect(nextCellStateForCell(0, 0, 0)).toBe(1);
-		expect(1).toBe(1)
+	it("should throw exception if no neighbors", () => {
+		expect(nextCellStateForCell(0, [[]])).toBe(1)
 	})
 })
+
+function nextCellStateForCell(arg0: number, arg1: number[][]): number {
+	// __AUTO_GENERATED_PRINT_VAR_START__
+	console.log("nextCellStateForCell arg1: %s", arg1) // __AUTO_GENERATED_PRINT_VAR_END__
+	// __AUTO_GENERATED_PRINT_VAR_START__
+	console.log("nextCellStateForCell arg0: %s", arg0) // __AUTO_GENERATED_PRINT_VAR_END__
+	throw new Error("Function not implemented.")
+}
