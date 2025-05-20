b2d3de7643f9d4a8342f33ccc2b2bd19e43d9578
koleh.david <koleh.david@aerometrex.com.au>
2025-05-20 15:11:58 +0930

    Revert "temp"
    This reverts commit a02c0ac9f29e6e88337b547def05c8c4f7d2b506.

--- src/game-logic/cell-logic.spec.ts
+++ src/game-logic/cell-logic.spec.ts
@@ -1,7 +1,6 @@
 describe("nextCellStateForCell", () => {
 	it("should return the next state for a cell", () => {
 		// expect(nextCellStateForCell(0, 0, 0)).toBe(1);
-		const temp = 2 / "1"
-		expect(temp).toBe(1)
+		expect(2 + "1").toBe(1)
 	})
 })
