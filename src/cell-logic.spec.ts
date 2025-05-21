37a73e8593d52c2b6ee24ed96d576b2dfc2af7b0
koleh.david <koleh.david@aerometrex.com.au>
2025-05-21 14:33:32 +0930

    t return dead

--- src/cell-logic.spec.ts
+++ src/cell-logic.spec.ts
@@ -3,6 +3,10 @@ import { InvalidGameDimensions, stateForCell } from "./cell-logic"
 
 describe("stateForCell", () => {
 	test("throws error if game dimensions invalid", () => {
-		expect(() => stateForCell(0, [[]])).toThrow(InvalidGameDimensions)
+		expect(() => stateForCell([0, 0], [[]])).toThrow(InvalidGameDimensions)
+	})
+
+	test("returns dead for no live neighbors", () => {
+		expect(stateForCell([0, 0], [[0]])).toBe(0)
 	})
 })
--- src/cell-logic.ts
+++ src/cell-logic.ts
@@ -1,5 +1,8 @@
 export class InvalidGameDimensions extends Error {}
 
 export function stateForCell(arg0: number, arg1: never[][]): any {
-	throw new InvalidGameDimensions("Invalid game dimensions")
+	if (arg1.length <= 1 && !arg1[0].length) {
+		throw new InvalidGameDimensions("Invalid game dimensions")
+	}
+	return 0
 }
