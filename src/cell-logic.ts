4116d5d1f4539c33cf2a6c2f32148a07fe60a06b
koleh.david <koleh.david@aerometrex.com.au>
2025-05-21 15:27:37 +0930

    r more fixes

--- src/cell-logic.ts
+++ src/cell-logic.ts
@@ -4,5 +4,7 @@ export interface Cell {
 }
 
 export function newStateForCell(cell: Cell): any {
+	// __AUTO_GENERATED_PRINT_VAR_START__
+	console.log(" cell: %s", cell) // __AUTO_GENERATED_PRINT_VAR_END__
 	return 0
 }
