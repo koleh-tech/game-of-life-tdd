9c37f89df74e06f3ae25bea3148e7727ec03e30d
koleh.david <koleh.david@aerometrex.com.au>
2025-05-22 10:47:50 +0930

    t final assertion

--- src/cell-logic.spec.ts
+++ src/cell-logic.spec.ts
@@ -13,7 +13,7 @@ describe("stateForCell", () => {
 
 	test("3. Any live cell with two or three live neighbours lives on to the next generation.", () => {
 		expect(newStateForCell({ state: 1, neighbors: [1, 1, 0] }).state).toBe(1)
-		// expect(newStateForCell({ state: 1, neighbors: [1, 1, 1] }).state).toBe(1)
+		expect(newStateForCell({ state: 1, neighbors: [1, 1, 1] }).state).toBe(1)
 	})
 
 	test.skip("4. Any dead cell with exactly three live neighbours becomes a live cell.", () => {
