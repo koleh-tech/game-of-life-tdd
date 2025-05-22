6d33bfe14105a5d19a1e84c940a837a226aa3608
koleh.david <koleh.david@aerometrex.com.au>
2025-05-22 10:27:37 +0930

    r extract func

--- src/cell-logic.ts
+++ src/cell-logic.ts
@@ -3,9 +3,13 @@ export interface Cell {
 	neighbors: number[]
 }
 
+function isAlive(cell: Cell) {
+	return cell.neighbors.length === 2 || cell.neighbors.length === 3
+}
+
 export function newStateForCell(cell: Cell) {
 	return {
 		...cell,
-		state: cell.neighbors.length === 2 || cell.neighbors.length === 3 ? 1 : 0,
+		state: isAlive(cell) ? 1 : 0,
 	}
 }
